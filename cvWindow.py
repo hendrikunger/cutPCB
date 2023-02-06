f48f8399c882e9c73ffeda2cf7339dd3f874e535
hendrik.unger@mb.tu-chemnitz.de <hendrik.unger@mb.tu-chemnitz.de>
2023-02-06 16:15:53 +0100

    Created Gui Tool

---
+++ cvWindow.py
@@ -0,0 +1,142 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+
+imageIndex = 0
+imageOnDisplay = None
+
+
+def imcrop(img, bbox):
+   x1, y1, x2, y2 = bbox
+   if x1 < 0 or y1 < 0 or x2 > img.shape[1] or y2 > img.shape[0]:
+        img, x1, x2, y1, y2 = pad_img_to_fit_bbox(img, x1, x2, y1, y2)
+   return img[y1:y2, x1:x2, :]
+
+def pad_img_to_fit_bbox(img, x1, x2, y1, y2):
+   img = cv2.copyMakeBorder(img, - min(0, y1), max(y2 - img.shape[0], 0), -min(0, x1), max(x2 - img.shape[1], 0),cv2.BORDER_CONSTANT, None, (255, 0, 0))
+   y2 += -min(0, y1)
+   y1 += -min(0, y1)
+   x2 += -min(0, x1)
+   x1 += -min(0, x1)
+   return img, x1, x2, y1, y2
+
+path = os.path.join(os.path.dirname(__file__), "parts")
+os.makedirs(path, exist_ok=True) 
+
+#imagename = "NG22_22"
+imagename = "NG5_5"
+#imagename = "NG11_11"
+#imagename = "NG14_14"
+
+
+
+extension = ".jpg"
+
+R = cv2.imread("R"+"_"+imagename+extension,cv2.IMREAD_GRAYSCALE)
+G = cv2.imread("G"+"_"+imagename+extension,cv2.IMREAD_GRAYSCALE)
+B = cv2.imread("B"+"_"+imagename+extension,cv2.IMREAD_GRAYSCALE)
+
+#color = cv2.UMat(cv2.merge([B, G, R]))
+color = cv2.merge([B, G, R])
+
+
+col_names = ["index", "X1", "Y1", "Klasse", "Flaeche", "X2", "X3", "Y2", "Y3", "F1", "F2"]
+df=pd.read_csv(imagename+".ngt", sep=',',index_col=0, header=None, names=col_names, skiprows=2)
+
+annotated = color.copy()
+
+for row in df.itertuples():
+    cv2.rectangle(annotated, (row.X2, row.Y2), (row.X3, row.Y3), (255, 0, 255),1)
+    cv2.putText(annotated, str(row.F1), (row.X1+10, row.Y1+10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+
+
+
+   
+
+
+cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
+
+def none_callback(*args):
+    displayNextImage()
+
+def b1_callback(*args):
+    displayNextImage(1)
+
+def b2_callback(*args):
+    displayNextImage(2)
+
+def b3_callback(*args):
+    displayNextImage(3)
+
+def b4_callback(*args):
+    displayNextImage(4)
+
+def b5_callback(*args):
+    displayNextImage(5)
+
+def b6_callback(*args):
+    displayNextImage(6)
+
+def b7_callback(*args):
+    displayNextImage(7)
+
+def b8_callback(*args):
+    displayNextImage(8)
+
+def b9_callback(*args):
+    displayNextImage(9)
+
+def b10_callback(*args):
+    displayNextImage(10)
+
+cv2.createButton("Kein Fehler",none_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_1",b1_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_2",b2_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_3",b3_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_4",b4_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_5",b5_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_6",b6_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_7",b7_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_8",b8_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_9",b9_callback,None,cv2.QT_PUSH_BUTTON,1)
+cv2.createButton("Typ_10",b10_callback,None,cv2.QT_PUSH_BUTTON,1)
+
+
+
+def displayNextImage(Imagetype = None):
+
+    global imageIndex
+    global imageOnDisplay
+
+    cv2.displayStatusBar('Display', f"{imageIndex}/{len(df.index)-1}")
+
+    if imageIndex >= len(df.index - 1):
+        cv2.displayStatusBar('Display', "Ende erreicht", 1000)
+        return
+    
+    if Imagetype:
+        cv2.imwrite(os.path.join(path,f"{imagename}_{imageIndex}_{str(Imagetype)}.jpg"), imageOnDisplay, [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+    topleft = np.array([df.iloc[imageIndex]['X1'], df.iloc[imageIndex]['Y1']])
+    topleft = np.floor_divide(topleft, 200) * 200
+    topleft = topleft -200
+    bottomright = topleft + 600
+
+    imageOnDisplay = imcrop(color, (topleft[0], topleft[1], bottomright[0], bottomright[1]))
+    cv2.imshow('Display', imcrop(annotated, (topleft[0], topleft[1], bottomright[0], bottomright[1])))
+    imageIndex = imageIndex + 1
+    #cv2.imwrite(os.path.join(path,f"{row.Index}.jpg"), output, [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+displayNextImage()
+
+while(True):
+
+
+    key = cv2.waitKey(20) & 0xff
+    if key==ord('q') or key == 27:
+        
+        break
+
+cv2.destroyAllWindows()
